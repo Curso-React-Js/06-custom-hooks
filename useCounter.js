eed6d8b29df662c3ad6c21aca353e8bb7c1cd42e
Luis Angel <angelfgdeveloper@gmail.com>
2023-03-05 15:10:52 -0600

    Primer commit

---
+++ useCounter.js
@@ -0,0 +1,26 @@
+// use es un estandar antes del nombre del archivo
+// como userCouunter.js
+
+import { useState } from 'react';
+
+export const useCounter = ( initialValue = 10 ) => {
+
+  const [ counter, setCounter ] = useState(initialValue);
+
+  const increment = ( value = 1 ) => setCounter( (current) => current + value);
+  const decrement = ( value = 1 ) => {
+    if (counter <= 0) return;
+
+    setCounter((current) => current - value);
+  }
+  const reset = () => setCounter(initialValue);
+
+  return {
+    counter,
+
+    // Methods
+    increment,
+    decrement,
+    reset
+  }
+}
